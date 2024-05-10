51efdda015d133e2fc4d1039336e20bd79ca9709
VyBoM <slava2006.150@gmail.com>
2024-05-10 14:51:09 +1000

    Добавили функционал обновления данных2

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
-const { sendAllUsers, sendUserUpdated } = require("../controllers/users");
+const { sendAllUsers, sendUserUpdated, sendUserCreated } = require("../controllers/users");
 const { findAllUsers, createUser, updateUser, checkEmptyNameAndEmail } = require("../middlewars/users");
 
 const usersRouter = require("express").Router();
 
 usersRouter.get("/users", findAllUsers, sendAllUsers);
-usersRouter.post("/users", checkEmptyNameAndEmail, findAllUsers, createUser);
+usersRouter.post("/users", checkEmptyNameAndEmail, findAllUsers, createUser, sendUserCreated);
 usersRouter.put("/users/:id", checkEmptyNameAndEmail, updateUser, sendUserUpdated);
 
 module.exports = usersRouter;
