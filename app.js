ec681b35a9564f206abaeee4746fa2a35826fb1d
VyBoM <slava2006.150@gmail.com>
2024-05-16 09:16:20 +1000

    добавили api, переместили роуты и изменили фронтентд(Public).

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const usersRouter = require("./routes/users");
-const categoriesRouter = require("./routes/categories");
-const gamesRouter = require("./routes/games");
 
 const connectToDatabase = require("./database/connect");
 const cors = require("./middlewars/cors");
-
-
+const apiRouter = require("./routes/api");
 
 const PORT = 3000;
 
 const app = express();
 connectToDatabase();
 
-app.use(cors, bodyParser.json(), express.static(path.join(__dirname, "public")), usersRouter, categoriesRouter, gamesRouter);
+app.use(cors, bodyParser.json(), express.static(path.join(__dirname, "public")), apiRouter);
 
 app.listen(PORT, () => {
 	console.log(`Сервер запущен на http://localhost:${PORT}`);
