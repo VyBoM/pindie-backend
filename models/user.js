f5fda7051a053411719afeba43fe84a45de6b660
VyBoM <slava2006.150@gmail.com>
2024-05-16 11:43:53 +1000

    Добавленая авторизация

--- models/user.js
+++ models/user.js
@@ -16,4 +16,22 @@ const userSchema = new mongoose.Schema({
 	}
 });
 
+userSchema.statics.findUserByCredentials = function(email, password) {
+	return this
+    .findOne({email})
+    .then(user => {
+        if (!user) {
+            return Promise.reject(new Error('Неправильная почта или пароль'));
+        }
+
+        return bcrypt.compare(password, user.password).then(matched => {
+            if (!matched) {
+                return Promise.reject(new Error("Неправильная почта или пароль"));
+            }
+
+            return user;
+        })
+		})
+}
+
 module.exports = mongoose.model("user", userSchema);
